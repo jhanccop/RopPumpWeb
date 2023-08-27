43a5d7812936677d2b64ed09b990dd59d2ab7f4a
jhanccop <jhanccop@gmail.com>
2023-08-27 16:16:33 -0500

    deploymentv1

--- RopPumpWeb/settings/base.py
+++ RopPumpWeb/settings/base.py
@@ -115,7 +115,7 @@ DATETIME_FORMAT = '%d-%m-%Y %H:%M:%S'
 LANGUAGE_CODE = 'en-us'
 
 #TIME_ZONE = 'UTC'
-TIME_ZONE = 'America/Monterrey'
+TIME_ZONE = 'America/Lima'
 
 USE_I18N = True
 
--- RopPumpWeb/settings/local.py
+++ RopPumpWeb/settings/local.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','*']
+ALLOWED_HOSTS = ['0.0.0.0','*','18.231.31.156']
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
