733fac5a2b9aa833370e707b1cf98b745df07122
jhanccop <jhanccop@gmail.com>
2025-05-14 23:35:45 -0500

    debug false

--- RopPumpWeb/settings/local.py
+++ RopPumpWeb/settings/local.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = ["0.0.0.0",'24.199.125.52',"*","24.144.68.190","192.168.3.56"]
 ALLOWED_HOSTS = ['24.199.125.52',"192.168.3.31","localhost","datagreensolutions.com","www.datagreensolutions.com"]
