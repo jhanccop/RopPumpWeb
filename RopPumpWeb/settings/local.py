b58ff8b4b175e8f24d6e22111d4f6b583c5f309b
jhanccop <jhanccop@gmail.com>
2026-07-19 15:14:44 -0500

    bug: correct

--- RopPumpWeb/settings/local.py
+++ RopPumpWeb/settings/local.py
@@ -33,6 +33,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR.child('static')]
 
 STATIC_ROOT = BASE_DIR.child('staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.child('media')
 
